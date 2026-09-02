827b8fcf893a9b644ab3cfeed36bcf5c76e71728
saviobandeira <savio-bandeira@hotmail.com>
2026-09-02 18:14:26 -0300

    First commit

---
+++ src/main/java/com/devsuperior/dscommerce/entities/Order.java
@@ -0,0 +1,31 @@
+package com.devsuperior.dscommerce.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Column;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_order")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant moment;
+    private OrderStatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private User client;
+
+
+}
